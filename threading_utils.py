777aee62b80765e1bed81835550cf51cbdc466cd
SeanLiJK <jingkuan.li@gmail.com>
2018-05-08 22:40:46 +0800

    老师给的线程类工具及简单演示使用

---
+++ threading_utils.py
@@ -0,0 +1,39 @@
+#coding:gbk
+
+import threading
+import time
+
+class FuncThread(threading.Thread):
+
+    def __init__(self, func, *args, **kwargs):
+        super(FuncThread, self).__init__()
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.finished = False
+        self.result = None
+
+    def run(self):
+        self.result = self.func(*self.args, **self.kwargs)
+        self.finished = True
+
+    def is_finished(self):
+        return self.finished
+
+    def get_result(self):
+        return self.result
+
+def do_in_thread(func, *args, **kwargs):
+    ft = FuncThread(func, *args, **kwargs)
+    ft.start()
+    return ft
+
+def sleep(n):
+    time.sleep(n)
+    print('sleep %s second' % n)
+    return 'money'
+
+t = do_in_thread(sleep, 2)
+time.sleep(2.5)
+var = t.get_result()
+print(var)
